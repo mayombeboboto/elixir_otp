18ccbde97ceec026fca9a3cab6b88ead3a965cf2
mayombeboboto <mayombegradi@gmail.com>
2023-06-09 17:08:18 +0200

    Blitzy demo

---
+++ Chap08/blitzy_v1/mix.exs
@@ -0,0 +1,29 @@
+defmodule Blitzy.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :blitzy,
+      version: "0.1.0",
+      elixir: "~> 1.12",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger, :httpoison, :timex]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:httpoison, "~> 2.0"},
+      {:timex, "~> 3.0"},
+      {:tzdata, "~> 1.1"}
+    ]
+  end
+end
