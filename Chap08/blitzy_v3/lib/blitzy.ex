a241fb9bdd5f86bc4a6b5b25aae842b9a11d0748
mayombeboboto <mayombegradi@gmail.com>
2023-06-20 10:00:11 +0200

    Continuation...

--- Chap08/blitzy_v3/lib/blitzy.ex
+++ Chap08/blitzy_v3/lib/blitzy.ex
@@ -3,45 +3,4 @@ defmodule Blitzy do
   Documentation for `Blitzy`.
   """
 
-  def run(number_of_workers, url) when number_of_workers > 0 do
-    worker_fun = fn -> Blitzy.Worker.start(url) end
-
-    1..number_of_workers
-    |> Enum.map(fn _value -> Task.async(worker_fun) end)
-    |> Enum.map(&Task.await(&1, :infinity))
-    |> parse_results()
-  end
-
-  defp parse_results(results) do
-    {successes, _failures} = split_results(results)
-
-    total_workers = Enum.count(results)
-    total_success = Enum.count(successes)
-    total_failure = total_workers - total_success
-
-    data = successes |> Enum.map(fn {:ok, time} -> time end)
-    average_time = average_time(data)
-    longest_time = Enum.max(data)
-    shortest_time = Enum.min(data)
-
-    IO.puts"""
-    Total workers:     #{total_workers}
-    Successful reqs:   #{total_success}
-    Failed res:        #{total_failure}
-    Average  (msecs):  #{average_time}
-    Longest  (msecs):  #{longest_time}
-    Shortest (msecs):  #{shortest_time}
-    """
-  end
-
-  defp split_results(results) do
-    fun = fn {:ok, _result} -> true
-             _other -> false end
-    Enum.split_with(results, fun)
-  end
-
-  defp average_time(list) do
-    sum = Enum.sum(list)
-    if sum > 0, do: sum / Enum.count(list), else: 0
-  end
 end
--- Chap08/blitzy_v3/lib/blitzy/CLI.ex
+++ Chap08/blitzy_v3/lib/blitzy/CLI.ex
@@ -3,38 +3,95 @@ defmodule Blitzy.CLI do
   require Logger
 
   def main(args) do
+    Application.get_env(:blitzy, :master_node)
+      |> Node.start
+
+    Application.get_env(:blitzy, :slave_nodes)
+      |> Enum.each(&Node.connect(&1))
+
     args
-    |> parse_args()
-    |> process_options()
+      |> parse_args
+      |> process_options([node()|Node.list()])
   end
 
   defp parse_args(args) do
-    options = [aliases: [n: :request],
-               strict: [request: :integer]]
-
-    OptionParser.parse(args, options)
+    OptionParser.parse(args, aliases: [n: :requests],
+                              strict: [requests: :integer])
   end
 
-  defp process_options(options, _nodes \\ []) do
+  defp process_options(options, nodes) do
     case options do
-      {[requests: _n], [_url], []} ->
-        :ok
+      {[requests: n], [url], []} ->
+        do_requests(n, url, nodes)
+
       _other ->
         do_help()
+
     end
   end
 
+  defp do_requests(n_requests, url, nodes) do
+    Logger.info "Pummelling #{url} with #{n_requests} requests"
+
+    total_nodes  = Enum.count(nodes)
+    req_per_node = div(n_requests, total_nodes)
+
+    nodes
+    |> Enum.flat_map(fn node ->
+         1..req_per_node |> Enum.map(fn _ ->
+           Task.Supervisor.async({TasksSupervisor, node}, Blitzy.Worker, :start, [url])
+         end)
+       end)
+    |> Enum.map(&Task.await(&1, :infinity))
+    |> parse_results
+  end
+
   defp do_help() do
-    IO.puts"""
+    IO.puts """
     Usage:
     blitzy -n [requests] [url]
 
     Options:
-    -n, [--requests]            # Number of requests
+    -n, [--requests]      # Number of requests
 
     Example:
     ./blitzy -n 100 http://www.bieberfever.com
     """
     System.halt(0)
+    :ok
+  end
+
+  defp parse_results(results) do
+    {successes, _failures} =
+      results
+        |> Enum.split_with(fn result ->
+             case result do
+               {:ok, _value} -> true
+               _other -> false
+           end
+         end)
+
+    total_workers = Enum.count(results)
+    total_success = Enum.count(successes)
+    total_failure = total_workers - total_success
+
+    data = successes |> Enum.map(fn {:ok, time} -> time end)
+    average_time  = average(data)
+    longest_time  = Enum.max(data)
+    shortest_time = Enum.min(data)
+
+    IO.puts """
+    Total workers    : #{total_workers}
+    Successful reqs  : #{total_success}
+    Failed reqs      : #{total_failure}
+    Average (msecs)  : #{average_time}
+    Longest (msecs)  : #{longest_time}
+    Shortest (msecs) : #{shortest_time}
+    """
+  end
+
+  defp average(list) do
+    sum = Enum.sum(list)
+    if sum > 0, do: sum / Enum.count(list), else: 0
   end
 end
