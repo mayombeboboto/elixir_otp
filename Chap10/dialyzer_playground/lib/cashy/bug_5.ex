69e0c141329f12062071bc037ba381df01b26faf
mayombeboboto <mayombegradi@gmail.com>
2023-07-11 19:27:08 +0200

    Type spec

--- Chap10/dialyzer_playground/lib/cashy/bug_5.ex
+++ Chap10/dialyzer_playground/lib/cashy/bug_5.ex
@@ -1,13 +1,13 @@
 defmodule Cashy.Bug5 do
+  @type currency() :: :sgd | :usd
+
+  @spec convert(currency(), currency(), integer() | float()) :: float()
   def convert(:sgd, :usd, amount) do
     amount * 0.70
   end
 
+  @spec amount({:value, number()}) :: number()
   def amount({:value, value}) do
     value
-  end
-
-  def run do
-    convert(:sgd, :usd, amount({:value, :one_million_dollars}))
   end
 end
